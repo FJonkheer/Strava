334fdffe235a12cf6953cc852c2599e3f897ec47
Florian <flo.jonkheer@hotmail.de>
2020-01-07 18:39:42 +0100

    Initial Commit

--- src/main/main.go
+++ src/main/main.go
@@ -25,6 +25,7 @@ func loadPage(title string) (*Page, error) {
 	}
 	return &Page{Title: title, Body: body}, nil
 }
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/"):]
 	p, _ := loadPage(title)
@@ -38,30 +39,9 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	p.save()
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
+
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/save/", saveHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
-	/*http.HandleFunc("/view/",viewHandler)
-	http.HandleFunc("/edit/",editHandler)*/
 }
-
-/*
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, _ := template.ParseFiles(tmpl + ".html")
-	t.Execute(w, p)
-}
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	renderTemplate(w,"view",p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w,"edit",p)
-}*/
