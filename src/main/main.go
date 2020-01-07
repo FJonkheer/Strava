ef6d9c3aabaf836a9cd3197b68a5b8ab881927c0
Julian <schwart38@gmail.com>
2020-01-07 20:47:24 +0100

    meins

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/md5"
 	"encoding/csv"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -55,37 +57,63 @@ func main() {
 
 func login(w http.ResponseWriter, r *http.Request) {
 	uname := r.FormValue("uname")
-	pword := r.FormValue("pword")
-	if len(uname) > 0 && len(pword) > 0 {
-		os.Open("Test.csv")
-		if fileExists("Test.csv") {
-			empData := [][]string{
-				{uname, pword}}
-			csvFile, err := os.OpenFile("Test.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-			if err != nil {
-				log.Fatalf("failed opening file: %s", err)
-			}
-			csvwriter := csv.NewWriter(csvFile)
-			for _, empRow := range empData {
-				_ = csvwriter.Write(empRow)
-			}
-			csvwriter.Flush()
-			csvFile.Close()
-		} else {
-			empData := [][]string{
-				{"uname", "pword"},
-				{uname, pword}}
-			csvFile, err := os.Create("Test.csv")
-			if err != nil {
-				log.Fatalf("failed creating file: %s", err)
-			}
-			csvwriter := csv.NewWriter(csvFile)
-			for _, empRow := range empData {
-				_ = csvwriter.Write(empRow)
-			}
-			csvwriter.Flush()
-			csvFile.Close()
+	password := r.FormValue("pword")
+	salt := "15967"
+	isGone := false
+	if len(uname) > 0 && len(password) > 0 {
+		lines, err := ReadCsv("Test.csv")
+		if err != nil {
+			panic(err)
 		}
+
+		for _, line := range lines {
+			data := CsvLine{
+				Column1: line[0],
+				Column2: line[1],
+			}
+			if data.Column1 == uname {
+				isGone = true
+			}
+
+		}
+		if isGone {
+			fmt.Fprintf(w, "<div>%s</div>", "Username schon vorhanden")
+		} else {
+			password = password + salt
+			pword := GetMD5Hash(password)
+			os.Open("Test.csv")
+			if fileExists("Test.csv") {
+				empData := [][]string{
+					{uname, pword}}
+				csvFile, err := os.OpenFile("Test.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+				if err != nil {
+					log.Fatalf("failed opening file: %s", err)
+				}
+				csvwriter := csv.NewWriter(csvFile)
+				for _, empRow := range empData {
+					_ = csvwriter.Write(empRow)
+				}
+				csvwriter.Flush()
+				csvFile.Close()
+			} else {
+				password = password + salt
+				pword := GetMD5Hash(password)
+				empData := [][]string{
+					{"uname", "pword"},
+					{uname, pword}}
+				csvFile, err := os.Create("Test.csv")
+				if err != nil {
+					log.Fatalf("failed creating file: %s", err)
+				}
+				csvwriter := csv.NewWriter(csvFile)
+				for _, empRow := range empData {
+					_ = csvwriter.Write(empRow)
+				}
+				csvwriter.Flush()
+				csvFile.Close()
+			}
+		}
+
 	} else {
 		fmt.Fprintf(w, "<div>%s</div>", "Username oder Passwort zu kurz")
 	}
@@ -97,4 +125,32 @@ func fileExists(filename string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+type CsvLine struct {
+	Column1 string
+	Column2 string
+}
+
+func ReadCsv(filename string) ([][]string, error) {
+
+	// Open CSV file
+	f, err := os.Open(filename)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+
+	// Read File into a Variable
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return lines, nil
 }
