4021dff356668ae29e0d2c5721074917f8c15cd9
Florian <flo.jonkheer@hotmail.de>
2020-01-11 15:58:26 +0100

    Moved maintest

--- src/main_test.go
+++ src/main_test.go
@@ -1,8 +1,6 @@
-package Tests
+package main
 
 import (
-	"Helper"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 
 	"Handler"
@@ -38,16 +36,9 @@ func LoginTest(t *testing.T) {
 		t.Errorf("handler returned wrong header: got %v want %v",
 			req.Header.Get("uname"), "Test")
 	}
-
 }
 
 func Test(t *testing.T) {
-	assert.Equal(t, Helper.GetMD5Hash("Hello"), "8b1a9953c4611296a827abf8c47804d7", "")
-	assert.NotEqual(t, Helper.GetMD5Hash("Test"), "8b1a9953c4611296a827abf8c47804d7", "")
-	assert.Equal(t, Helper.Validation(20, 20, 20), "f", "")
-	assert.Equal(t, Helper.Validation(6, 10, 20), "l", "")
-	assert.Equal(t, Helper.Latlongtodistance(0, 0, 0, 0), float64(0), "")
-	assert.Equal(t, Helper.Latlongtodistance(10, 10, 20, 20), float64(1.5464880483491938e+06), "")
 	HttpTests(t)
 	LoginTest(t)
 }
