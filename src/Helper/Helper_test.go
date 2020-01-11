915f42425218eda381fe1f2f4d061dec6c7a589c
Florian <flo.jonkheer@hotmail.de>
2020-01-11 14:46:50 +0100

    Helper-Tests

--- src/Helper/Helper_test.go
+++ src/Helper/Helper_test.go
@@ -1,6 +1,7 @@
 package Helper
 
 import (
+	"encoding/csv"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
@@ -18,16 +19,29 @@ func MD5_Test(t *testing.T) {
 func FileAccess_Test(t *testing.T) {
 	//Vorbereitung
 	assert.Equal(t, CreateFolders("testfolder"), nil)
+	var testfolder []string
+	testfolder = append(testfolder, "existantfile.csv")
 	testdatei := "testfolder/existantfile.csv"
-	os.Create(testdatei)
+	empData := [][]string{
+		{"test1", "test2"},
+		{"value1", "value2"},
+	}
+	csvdatei, _ := os.Create(testdatei)
+	csvwriter := csv.NewWriter(csvdatei)
+	for _, empRow := range empData {
+		_ = csvwriter.Write(empRow) //Und die Indexe geschrieben
+	}
+	csvwriter.Flush()
+	csvdatei.Close()
+	var teststruct UserFiles
 
 	assert.Equal(t, Validation(20, 20, 20), "f", "")
 	assert.Equal(t, Validation(6, 10, 20), "l", "")
 	assert.Equal(t, Latlongtodistance(0, 0, 0, 0, 0, 0), float64(0), "")
 	assert.Equal(t, Latlongtodistance(10, 10, 20, 20, 10, 0), float64(1.5464880483491938e+06), "")
 	assert.Equal(t, FileExists("testfolder/notexistentfile"), false)
-
-	//Aufräumen
+	//assert.Equal(t, interface{}(ReadCsv(testdatei)), empData)
 	assert.Equal(t, FileExists(testdatei), true)
-	assert.Equal(t, DeleteFiles(testdatei), nil)
+	assert.Equal(t, Scanforcsvfiles("testfolder"), testfolder)
+	assert.Equal(t, Parsecsvtostruct(testdatei), teststruct)
 }
